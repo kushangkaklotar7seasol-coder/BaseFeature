e658e4c0505f3f167ce2c8d58182de3706916232
kushangkaklotar7seasol-coder <kushangkaklotar.7seasol@gmail.com>
2026-08-10 11:13:39 +0530

    Project created

---
+++ BaseFeature/BaseFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseFeatureApp.swift
+//  BaseFeature
+//
+//  Created by Kushang kaklotar on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BaseFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
